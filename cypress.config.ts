f760b90cc9751fa90cf2b213a76b7767f8177750
Sergey <zenkovich_so@mail.ru>
2025-04-21 21:57:47 +0700

    #105: The testing environment

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,31 +1,18 @@
 import { defineConfig } from "cypress";
 import dbClientConfig from "./cypress/support/db-client-config";
-const { Client } = require("pg");
+import { Client } from "pg";
 
 export default defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
       on("task", {
-        async connectDB(query) {
+        async executeQuery({ sql }: { sql: string }) {
           const client = new Client(dbClientConfig);
           await client.connect();
-          const sql = `
-    INSERT INTO posts (id, title, content, authorId, createdAt, updatedAt, publishedAt, views)
-    VALUES (
-      'cuid1', -- уникальный идентификатор поста
-      'Example Post', -- заголовок поста
-      'This is an example post content.', -- содержимое поста
-      'user1', -- идентификатор автора поста
-      '2023-10-01T12:00:00Z', -- дата создания поста
-      '2023-10-01T12:00:00Z', -- дата последнего обновления поста
-      '2023-10-01T12:00:00Z', -- дата публикации поста
-      0 -- количество просмотров поста
-    );
-  `;
-          const res = await client.query();
+          await client.query(sql);
           await client.end();
-          return res.rows;
+          return null;
         },
       });
     },
--- cypress/e2e/posts/search.cy.ts
+++ cypress/e2e/posts/search.cy.ts
@@ -1,11 +1,33 @@
-import post from "../../fixtures/posts/simple-post.json";
-import user from "../../fixtures/users/simple-user.json";
-
 describe("Search posts", () => {
   it("Simple display list of posts", () => {
-    cy.task("connectDB", "SELECT NOW()").then((res) => {
-      cy.log(res[0].now);
+    cy.task("executeQuery", {
+      sql: getSql(),
+    }).then((e) => {
       cy.visit("/posts");
     });
   });
 });
+
+function getSql() {
+  return `
+    TRUNCATE TABLE "User" RESTART IDENTITY CASCADE;
+    TRUNCATE TABLE "Post" RESTART IDENTITY CASCADE;
+    INSERT INTO "User" (id, name, email, "createdAt", "updatedAt")
+      VALUES (
+        'user1',
+        'Simple user',
+        'test@mail.ru',
+        '2023-10-01T12:00:00Z',
+        '2023-10-01T12:00:00Z'
+      );
+      INSERT INTO "Post" (id, title, content, "authorId", "createdAt", "updatedAt")
+      VALUES (
+        'cuid1',
+        'Example Post 1',
+        'This is an example post content.',
+        'user1',
+        '2023-10-01T12:00:00Z',
+        '2023-10-01T12:00:00Z'
+      );
+  `;
+}
